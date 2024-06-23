9336af9803622b0dfeda4650809e5a831234bdb2
zhang999-h <2307143161@qq.com>
2024-06-23 14:25:18 +0800

    Part 3: resnet9

--- hw4/python/needle/optim.py
+++ hw4/python/needle/optim.py
@@ -25,7 +25,17 @@ class SGD(Optimizer):
 
     def step(self):
         ### BEGIN YOUR SOLUTION
-        raise NotImplementedError()
+        for parm in self.params:
+            if self.weight_decay != 0:
+                #
+                grad = parm.grad.data + self.weight_decay * parm.data
+            else:
+                grad = parm.grad.data
+            if parm not in self.u:
+                self.u[parm] = 0
+            self.u[parm] = self.momentum * self.u[parm] + (1 - self.momentum) * grad
+
+            parm.data = parm.data - self.lr * self.u[parm]
         ### END YOUR SOLUTION
 
     def clip_grad_norm(self, max_norm=0.25):
@@ -60,5 +70,23 @@ class Adam(Optimizer):
 
     def step(self):
         ### BEGIN YOUR SOLUTION
-        raise NotImplementedError()
+        self.t = self.t + 1
+        for param in self.params:
+            if self.weight_decay != 0:
+                #
+                grad = param.grad.data + self.weight_decay * param.data
+            else:
+                grad = param.grad.data
+            # m是一阶矩（动量）估计
+            if param not in self.m:
+                self.m[param] = 0
+            if param not in self.v:
+                self.v[param] = 0
+            self.m[param] = self.beta1 * self.m[param] + (1 - self.beta1) * grad
+            self.v[param] = self.beta2 * self.v[param] + (1 - self.beta2) * grad * grad
+            m_hat = (self.m[param] / (1 - np.power(self.beta1, self.t))).data
+            v_hat = (self.v[param] / (1 - np.power(self.beta2, self.t))).data
+            param.data = param.data - self.lr * m_hat / (
+                ndl.ops.power_scalar(v_hat, 0.5) + self.eps)
+
         ### END YOUR SOLUTION
